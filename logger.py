a092f06ac94836c2cdc7073ac3e02e10fb1c729f
Radoslaw Burkacki <radekb333@gmail.com>
2021-03-15 00:15:15 +0000

    logs dir is created on start up if not exists

--- logger.py
+++ logger.py
@@ -2,13 +2,18 @@
 import logging
 import time
 import datetime
+import os.path
+from os import path
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
+
+if not path.exists('logs'):
+    os.mkdir('logs')
+
 fh = logging.FileHandler('logs/test.log')
 fh.setLevel(logging.INFO) # or any level you want
 logger.addHandler(fh)
-
 
 def log(msg):
     now = datetime.datetime.now()
